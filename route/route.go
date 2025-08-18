08b174349b1bdb0843193639710d1c4c5d58dccc
sonjungchan <rnrmfjc@gmail.com>
2025-08-18 20:47:39 +0900

    feat(cors): add cors config

--- route/route.go
+++ route/route.go
@@ -20,3 +20,14 @@ func InitRoute() *gin.Engine {
 
 	return r
 }
+
+func corsConfig(r *gin.Engine) {
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+}
