f5389428a899f7a177dab15af548254e488e717a
sonjungchan <rnrmfjc@gmail.com>
2025-08-20 01:07:43 +0900

    feat(collect): add mutex in repository

--- repository/agent_info/agent_info_repository.go
+++ repository/agent_info/agent_info_repository.go
@@ -2,10 +2,12 @@ package agent_info
 
 import (
 	common "morpher-controller/common/types"
+	"sync"
 )
 
 type AgentInfoRepository struct {
 	agentInfoMap map[string]common.AgentSystemInfo
+	mutex        sync.RWMutex
 }
 
 func NewAgentInfoRepository() *AgentInfoRepository {
@@ -15,10 +17,16 @@ func NewAgentInfoRepository() *AgentInfoRepository {
 }
 
 func (repository *AgentInfoRepository) Save(agentInfo common.AgentSystemInfo) {
+	repository.mutex.Lock()
+	defer repository.mutex.Unlock()
+
 	repository.agentInfoMap[agentInfo.ID] = agentInfo
 }
 
 func (repository *AgentInfoRepository) GetAll() []common.AgentSystemInfo {
+	repository.mutex.Lock()
+	defer repository.mutex.Unlock()
+
 	var agentSystemInfoArr []common.AgentSystemInfo
 	for _, value := range repository.agentInfoMap {
 		agentSystemInfoArr = append(agentSystemInfoArr, value)
